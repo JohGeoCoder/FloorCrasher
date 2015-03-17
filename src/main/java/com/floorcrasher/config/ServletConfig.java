2b0e1e8977cb5f7fceb8997d41b12f251f436524
John George <joecracko@gmail.com>
2015-03-17 17:40:16 -0400

    restructuring to remove XMLs and to get static files to deploy to JAR

--- src/main/java/com/floorcrasher/config/ServletConfig.java
+++ src/main/java/com/floorcrasher/config/ServletConfig.java
@@ -1,21 +1,33 @@
 package com.floorcrasher.config;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import com.floorcrasher.interceptor.BaseInterceptor;
 
 
 @Configuration
 @EnableWebMvc
 // declare where to find annotated controllers
 @ComponentScan({"com.floorcrasher"})
+@MapperScan("com.floorcrasher.mappers")
+@EnableTransactionManagement
 public class ServletConfig extends WebMvcConfigurerAdapter{
 	
 	@Bean
@@ -32,6 +44,10 @@ public class ServletConfig extends WebMvcConfigurerAdapter{
         return resolver;
     } 
     
+    /**
+     * Database Config
+     * @return
+     */
     @Bean(name = "dataSource")
 	public DriverManagerDataSource dataSource() {
 	    DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
@@ -41,4 +57,45 @@ public class ServletConfig extends WebMvcConfigurerAdapter{
 	    driverManagerDataSource.setPassword("password");
 	    return driverManagerDataSource;
 	}
+    
+    /**
+     * Database Config
+     * @return
+     */
+    @Bean
+    public DataSourceTransactionManager dataSourceTransactionManager(){
+    	DataSourceTransactionManager dataSourceTransactionManager = new DataSourceTransactionManager();
+        dataSourceTransactionManager.setDataSource(dataSource());
+        return dataSourceTransactionManager;
+    }
+    
+    /**
+     * Mybatis SQL session factory
+     * @return
+     * @throws Exception
+     */
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+      SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+      sqlSessionFactory.setDataSource(dataSource());
+      return (SqlSessionFactory) sqlSessionFactory.getObject();
+    }
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new BaseInterceptor());
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/assets/**").addResourceLocations("/assets/");
+		registry.addResourceHandler("/bootstrap/**").addResourceLocations("/bootstrap/");
+	}
+    
+	@Bean
+    public ResourceBundleMessageSource messageSource() {
+        ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+        source.setBasename("messages");
+        return source;
+    }
 }
