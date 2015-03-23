92cf41b0bbfd2e825a4fda78e189467ae0d6c0bb
John George <joecracko@gmail.com>
2015-03-23 16:23:35 -0400

    Further clean-up code for LoginController.java

--- src/main/java/com/floorcrasher/controller/LoginController.java
+++ src/main/java/com/floorcrasher/controller/LoginController.java
@@ -1,14 +1,10 @@
 package com.floorcrasher.controller;
 
-import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
